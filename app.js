4740c4b40a29e94dabdcae21261f6013cc5027dc
Burhan Shaheen <78025752+burhanrepos@users.noreply.github.com>
2021-09-10 16:18:05 +0500

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded( {extended: true}));
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname +"/signup.html");
+    res.sendFile(__dirname +"/index.html");
 });
 
 app.post("/",function(req,res){
